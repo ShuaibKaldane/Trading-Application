17ee6bfa11391c6a36102cfb3408142c7c6eb442
Shuaib Kaldane <shuaib.kaldane@vit.edu.in>
2025-11-09 13:20:15 +0530

    Data inserted

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3002;
-const url = process.env.MONGO_URL ;
+const url = process.env.MONGO_URL;
 
-app.listen(3002 , (req , res)=>{
-    console.log("Server Started")
-    mongoose.connect(url);
-    console.log("Database Connected");
-})
+
+app.listen(3002, (req, res) => {
+  console.log("Server Started");
+  mongoose.connect(url);
+  console.log("Database Connected");
+});
+
+
